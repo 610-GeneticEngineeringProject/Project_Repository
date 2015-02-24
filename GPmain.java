00ba793942955367c57285a4b7cffd4711b94ecc
Ben Andersen <bcandersen06@yahoo.com>
2015-02-24 11:47:40 -0600

    Initial Commit

---
+++ GPmain.java
@@ -0,0 +1,20 @@
+//********************************************************************
+//  GPmain.java       Software Engineering
+//  Created by: Benjamin Andersen
+//  Created: 2/24/2015
+//
+//  Genetic Programming algorithm to replicate a given target function
+//********************************************************************
+
+// Items to address (in particular order)
+/*
+- Set up binary tree fully (operand vs operator modes, etc)
+- Set up crossover
+- Set up fitness functionality
+- Maybe set up elitism
+- Set up mutation
+- User input for function
+- Set up git for this project
+*/
+
+// Step 1 - Have user input target function, and write back out for confirmation
